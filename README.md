731d3327f1d768e71d164f133074a5551c9d692b
Shaker Abdul <AShaker@academytrainee.com>
2020-10-17 21:54:03 +0100

    create added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # AssignmentOneFrontEnd
+ Please refer back to the Back End documentation for the full description of the project. 
+
+ This is the front end of the project which consists of utilising the backend to perform CRUD functionality. This includes taking the 3 inputs and creating a new object, displayed on the webpage, essentially acting as the read. User has ability to delete or update the object with inbuilt buttons on the object. Currently price can only be ammended.
---
+++ js/index.js
@@ -0,0 +1,22 @@
+const createForm = document.getElementById("createForm");
+
+createForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const data = {
+        name: this.name.value,
+        genre: this.genre.value,
+        price: this.price.value
+    }
+    fetch("http://localhost:8081/createGame", { //Make the create request, similar to postman
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': "application/json"
+        }
+    }).then(response => { // Receive response and then 
+        return response.json(); // Convert response body to a json string
+    }).then(data => { //json data from previous, then perform this function
+        console.log(data);
+         this.reset();
+    }).catch(error => console.log(error));
+});
